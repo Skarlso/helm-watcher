68ad3c14487cee8efd86b563a8d3051c1d53ab28
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-01-07 17:30:04 +0100

    Added skeleton of the watcher

---
+++ cache/in_memory_cache.go
@@ -0,0 +1,32 @@
+package cache
+
+import "sync"
+
+type HelmCache struct {
+	cache map[string]string
+	mut   *sync.RWMutex
+}
+
+func NewCache() *HelmCache {
+	return &HelmCache{
+		cache: make(map[string]string),
+	}
+}
+
+func (c *HelmCache) Add(key, value string) {
+	c.mut.Lock()
+	defer c.mut.Unlock()
+	c.cache[key] = value
+}
+
+func (c *HelmCache) Remove(key string) {
+	c.mut.Lock()
+	defer c.mut.Unlock()
+	delete(c.cache, key)
+}
+
+func (c *HelmCache) Get(key string) string {
+	c.mut.RLock()
+	defer c.mut.RUnlock()
+	return c.cache[key]
+}
